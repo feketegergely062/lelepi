e1a3e00b009b70442b7c0c7c58f56d4aeebdf6ac
Fekete Gergely <feketegergely062@gmail.com>
2023-04-27 13:23:36 +0200

    masodik

--- README.md
+++ README.md
@@ -1,4 +1,24 @@
 ## Fekete Gergely
 
 2. dolgozat megoldása 
-Feladat 0358 
+Feladat 0359 
+
+/*
+
+File: Readme.md
+Author: Fekete Gergely
+Copyright: 2023, Fekete Gergely
+Group: Szoft I/1/N
+Date: 2023-04-27
+Github: https://github.com/feketegergely062/lelepi
+Licenc: GNU GPL
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details:
+http://www.gnu.org/licenses/gpl.html 
+*/
--- src/App.java
+++ src/App.java
@@ -1,6 +1,37 @@
+/*
+* File: App.java
+* Author: Fekete Gergely
+* Copyright: 2023, Fekete Gergely
+* Group: Szoft 1 N
+* Date: 2023-04-27
+* Github:https://github.com/feketegergely062/lelepi
+* Licenc: GNU GPL
+*/
+import java.util.Scanner;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-        System.out.println("Fekete Gergely ");
+    public static void main(String[] args) {
+        System.out.println("Feladat: 0358");
+        System.out.print("Keszitette: Fekete Gergely, Szoft/1/N 2023-04-27\n ");
+        Scanner szamit = new Scanner(System.in);  
+        System.out.println("Add meg a paralepid A oldalat");
+
+        double Aold = szamit.nextDouble();
+        System.out.println("Add meg a paralepid B oldalat");
+
+        double Bold = szamit.nextDouble();
+        System.out.println("Add meg a paralepid C oldalat");
+
+        double Cold = szamit.nextDouble();
+        System.out.println("Az értéke: ");
+        
+        double terfogat = Aold*Bold*Cold;
+    
+        System.out.print(terfogat);
+
     }
 }
+
+
+    
+
